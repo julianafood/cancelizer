b62a2c8f143ddbd96b9c9e98797bf428fb04740e
or1979 <orobinson79@gmail.com>
2019-05-16 11:48:14 -0400

    2

--- recipesplus/src/components/OAuth/OAuth.js
+++ recipesplus/src/components/OAuth/OAuth.js
@@ -22,7 +22,7 @@ export default class OAuth extends Component {
 // Routinely checks the popup to re-enable the login button 
 // if the user closes the popup without authenticating.
 
-checkPopup() {
+checkPopup(); {
     const check = setInterval(() => {
         const { popup } = this
         if (!popup || popup.closed || popup.closed === undefined) {
@@ -32,31 +32,73 @@ checkPopup() {
     }, 1000)
 }
 
-   // Launches the popup by making a request to the server and then 
-  // passes along the socket id so it can be used to send back user 
-  // data to the appropriate socket on the connected client.
+// Launches the popup by making a request to the server and then 
+// passes along the socket id so it can be used to send back user 
+// data to the appropriate socket on the connected client.
 
-  openPopup() {
-      const { provider, socket } = this.props
-      const width = 600, height = 600
-      const left = (window.innerWidth /2) - (width /2)
-      const top = (window.innerHeight /2) - (height /2)
-      const url = `${API_URL}/${provider}?socketId=${socket.id}`
+openPopup(); {
+    const { provider, socket } = this.props
+    const width = 600, height = 600
+    const left = (window.innerWidth / 2) - (width / 2)
+    const top = (window.innerHeight / 2) - (height / 2)
+    const url = `${API_URL}/${provider}?socketId=${socket.id}`
 
-      return window.open(url, '',
-      `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${width}, height=${height}, top=${top}, left=${left}`
-      )
-  }
+    return window.open(url, '',
+        `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${width}, height=${height}, top=${top}, left=${left}`
+    )
+}
 
 
-  // Kicks off the processes of opening the popup on the server and listening 
-  // to the popup. It also disables the login button so the user can not 
-  // attempt to login to the provider twice.
-  startAuth(e) {
-      if(!this.state.disabled) {
-          e.preventDefault()
-          this.popup = this.openPopup()
-          this.checkPopup()
-          this.setState({disabled:'disabled'})
-      }
-  }
+// Kicks off the processes of opening the popup on the server and listening 
+// to the popup. It also disables the login button so the user can not 
+// attempt to login to the provider twice.
+startAuth(e); {
+    if (!this.state.disabled) {
+        e.preventDefault()
+        this.popup = this.openPopup()
+        this.checkPopup()
+        this.setState({ disabled: 'disabled' })
+    }
+}
+
+closeCard(); {
+    this.setState({ user: {} })
+}
+
+// Shows provider info for profile
+render(); {
+    const { name, photo} = this.state.user
+    const { provider } = this.props
+    const { disbaled } = this.state
+
+    return (
+    <div>
+        {name
+
+            ? <div className={'card'}>
+                <img src={photo} alt={name} />
+                <FontAwesome
+                    name={'times-circle'}
+                    className={'close'}
+                    onClick={this.closeCard.bind(this)}
+                />
+                <h4>{name}</h4>
+            </div>
+            : <div claseName={'button-wrapper fadein-fast'}>
+                <button
+                    onClick={this.startAuth.bind(this)}
+                    className={`${provider} ${disabled} button`}
+                >
+                    <FontAwesome
+                        name={provider}
+                    />
+                </button>
+            </div>
+
+        }
+    </div>
+        )
+}
+
+
+
