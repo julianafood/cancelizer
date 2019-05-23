3cd29d30095e9be1e0f1e2a3512d32631234a40d
or1979 <48077931+or1979@users.noreply.github.com>
2019-05-23 10:53:27 -0400

    Update index.js

--- recipesplus/src/components/Landing/index.js
+++ recipesplus/src/components/Landing/index.js
@@ -1,1 +1,14 @@
 import React from "react";
+
+const LandingPage = () => {
+    return (
+      
+  
+   <div>     
+ <h2> Landing Page</h2>
+  
+     </ div>
+    );
+  };
+
+export default LandingPage;
